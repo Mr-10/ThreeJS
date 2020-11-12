3faa65c40d615e9a9eae7b396154bb3290aa190d
Ashutosh Hundekar <ashutosh.hundekar@ip-192-168-43-67.eu-west-1.compute.internal>
2020-11-12 11:35:03 +0530

    added assets for the colour tray and options

---
+++ constants/constants.js
@@ -0,0 +1,50 @@
+export const texturesAndColors = [
+  {
+    texture: "./textures/fabric_.jpg",
+    size: [4, 4, 4],
+    shininess: 0,
+  },
+  {
+    texture: "./textures/denim_.jpg",
+    size: [3, 3, 3],
+    shininess: 0,
+  },
+  {
+    texture: "./textures/quilt_.jpg",
+    size: [6, 6, 6],
+    shininess: 0,
+  },
+  {
+    texture: "./textures/gray_leather.jpg",
+    size: [6, 6, 6],
+    shininess: 0,
+  },
+  {
+    texture: "./textures/leather3.jpg",
+    size: [6, 6, 6],
+    shininess: 0,
+  },
+  {
+    texture: "./textures/cloth.jpeg",
+    size: [6, 6, 6],
+    shininess: 0,
+  },
+  {
+    color: "5f6e78",
+  },
+  {
+    color: "acb4b9",
+  },
+  {
+    color: "DF9998",
+  },
+  {
+    color: "7C6862",
+  },
+  {
+    color: "A3AB84",
+  },
+  {
+    color: "D6CCB1",
+  },
+];
